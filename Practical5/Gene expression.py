0ae08c0ef290297c82385c90989d369f489b5631
TAB <2741826589@qq.com>
2026-03-18 15:21:18 +0800

    change

--- Practical5/Gene expression.py
+++ Practical5/Gene expression.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt 
 
-plt.ion() # I find that if I don't write down this, the code after the chart can't run unless I turn off the chart.
+#plt.ion() # I find that if I don't write down this, the code after the chart can't run unless I turn off the chart.
 Gene_expression = {"TP53":12.4,"EGFR":15.1,"BRCA1":8.2,"PTEN":5.3,"ESR1":10.7}
+print(Gene_expression)
 Gene_expression["MYC"]=11.6
 Gene_name=["MYC","TP53","EGFR","BRCA1","PTEN","ESR1"]
 Gene_level=[11.6,12.4,15.1,8.2,5.3,10.7]
@@ -10,7 +11,7 @@ plt.title("Gene expression")
 plt.ylabel("Expression level")
 plt.show()
 
-a=str(input('What kind of gene do you want to search?(You should take care of yhe capital and small letter)\n'))  #a is what the name the users input
+a= "MYC"  #a can be any other gene you are interested in
 if a in Gene_expression:
     print("exist")
     print("The expression is",Gene_expression[a])
@@ -18,5 +19,5 @@ else:
     print("Not exist(error)")
 
 b=sum(Gene_expression.values()) #b is the total number of all level 
-c=b/5 # c is the average number of all level
-print("The average level is",c)
+c=b/6 # c is the average number of all level
+print("The average level is %.2f"%c)
